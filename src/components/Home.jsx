d364c6d53e8dacd715eed4ac732eb06ba71fbb3b
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-02-14 15:01:29 +0530

    navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div class="card bg-dark text-white border-0">
+        <img src="/assets/bg.jfif" class="card-img" alt="background" height="450px" />
+              <div class="card-img-overlay d-flex flex-column justify-content-center">
+                  <div className="container">
+          <h5 class="card-title display-3 fw-bolder mb-0">New SESON ARRIVAL</h5>
+          <p class="card-text lead fs-2">
+                CHECK OUT ALL THE TRENDS
+          </p>
+         </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
