140f7e6b964af150df1a931d4959cbae164284f7
Gopi Mudumal <gopi.mudumal@perennialsys.com>
2022-10-07 11:17:37 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
         <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">
-              NEW SESON ARRIVAL
+              NEW SESSION ARRIVAL
             </h5>
             <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
           </div>
